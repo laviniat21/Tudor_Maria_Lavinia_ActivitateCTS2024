5dcd85ccef3cddb53457326dda4a74a5a1398677
laviniat21 <tudorlavinia21@gmail.com>
2024-05-08 18:11:57 +0300

    Seminar10

--- seminar10/src/problema14Template/main/Main.java
+++ seminar10/src/problema14Template/main/Main.java
@@ -8,11 +8,8 @@ public class Main {
     public static void main(String[] args) {
         SpitalPublic spitalPublic=new SpitalPublic("Coltea",2);
         Pacient pacient1=new Pacient("Gheorge");
-        pacient1.setStareSanatate(2);
         Pacient pacient2=new Pacient("Miky");
-        pacient2.setStareSanatate(3);
         Pacient pacient3=new Pacient("Ion");
-        pacient3.setStareSanatate(1);
 
         spitalPublic.internare(pacient1);
         spitalPublic.internare(pacient2);
